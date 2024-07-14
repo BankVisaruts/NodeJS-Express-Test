e2b2169024014684ff6bcf2b885a9d6a3d40633b
BankVisaruts <visarut.ik@gmail.com>
2024-07-14 15:26:33 +0700

    Edit App.js and Add index.html
    -Edit App.js
    -Add Index.html

--- app.js
+++ app.js
@@ -2,19 +2,26 @@
 import express from 'express';
 import chalk from 'chalk';
 import createDebug from 'debug';
-import morgan from 'morgan';
 const debug = createDebug('app');
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) =>{
     res.send('Hello borntoDev');
 })
 
 app.listen(port, ()=>{
-    //console.log("Listening on port %s", chalk.green(port)); //console
+    console.log("Listening on port %s", chalk.green(port)); //console
     debug("Listening on port" + chalk.green(" : " + port)); //debug
 })
