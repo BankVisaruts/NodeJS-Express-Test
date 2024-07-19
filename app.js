776904e92fe01382849cd8fb35c9203af190272f
BankVisaruts <visarut.ik@gmail.com>
2024-07-19 20:53:13 +0700

    Edit index icon
    Edit index.ejs icon

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("view engine", "ejs");
 
 app.get("/", (req,res) =>{
     //res.send('Hello BorntoDev'); //index
-    res.render('index',{username: 'Visarut', customer: [".Net C#", ".Net Core", "NodeJS"]});
+    res.render('index',{username: 'Visarut', customer: [".Net Core", "NodeJS", "Angular"]});
 })
 
 app.listen(PORT, ()=>{
